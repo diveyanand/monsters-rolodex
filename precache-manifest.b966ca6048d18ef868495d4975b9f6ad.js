cadd74bcd681a8b3f9f94eeec8acf84c2cbf2645
Divey Anand <diveya2@illinois.edu>
2020-01-19 18:18:00 -0600

    Updates

---
+++ precache-manifest.b966ca6048d18ef868495d4975b9f6ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e773feca40c18789acc3e52933b282ae",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "7b7b7175885add85caaf",
+    "url": "/mosters-rolodex/static/css/main.4bcb14be.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/mosters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mosters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b7b7175885add85caaf",
+    "url": "/mosters-rolodex/static/js/main.6435d740.chunk.js"
+  },
+  {
+    "revision": "33330621536c7180b173",
+    "url": "/mosters-rolodex/static/js/runtime-main.e4fe18ba.js"
+  }
+]);
